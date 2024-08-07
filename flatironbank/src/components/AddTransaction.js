79179a1c7dd3207457ecb0902f8b094402adc5af
kevinkamau <kevin.kamau2@student.moringaschool.com>
2024-08-08 01:32:25 +0300

    Fixed the add transaction function

--- flatironbank/src/components/AddTransaction.js
+++ flatironbank/src/components/AddTransaction.js
@@ -1,1 +1,13 @@
+// Added the add Transaction function here to pass it down as a prop
+function addTransaction(transaction) {
+    fetch('http://localhost:3000/transactions', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(transaction)
+  })
+  .then(response => response.json())
+  .then(data => setTransactions([...transactions, data]));
+}
 
