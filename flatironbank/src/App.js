e32c49592a0933aac6698e07589c624e1f927801
kevinkamau <kevin.kamau2@student.moringaschool.com>
2024-08-08 01:37:08 +0300

    Fixed errors within the app.js relating to the definition of addTransaction function

--- flatironbank/src/App.js
+++ flatironbank/src/App.js
@@ -8,7 +8,18 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [transactions, setTransactions] = useState([]);
 
-
+  //  Added the addTransaction function here to pass it down as a prop
+  function addTransaction(transaction) {
+    fetch('http://localhost:3000/transactions', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(transaction)
+  })
+  .then(response => response.json())
+  .then(data => setTransactions([...transactions, data]));
+}
 
   return (
     <div className="app">
